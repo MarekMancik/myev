92cf715b76a85f1174c6614f134b56fd6b58ae3e
unknown <you@example.com>
2024-03-05 22:06:51 +0100

    add tinyMCE part1

--- myev/urls.py
+++ myev/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('tinymce/', include('tinymce.urls')),
+    path('', include('article.ulrs')),
 ]
