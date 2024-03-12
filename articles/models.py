6ae23391f94164ad5045accf16f87938284ad038
unknown <you@example.com>
2024-03-12 21:50:14 +0100

    change text editor from tinyMCE to CKeditor

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from tinymce.models import HTMLField
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    remark = HTMLField(default="Napiš něco o sobě")
+    remark = RichTextField(null=True, blank=True)
 
     # def __str__(self):
     #     return self.name
--- myev/settings.py
+++ myev/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainapp',
     'articles',
-    'tinymce',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
